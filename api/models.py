7af335ed93bdeeb3d5926e22499bf928cc7a6be5
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-02-08 10:59:01 +0500

    feat: complete product, order, order items model

--- api/models.py
+++ api/models.py
@@ -30,3 +30,25 @@ class Order(models.Model):
     order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING
+    )
+    product = models.ManyToManyField(Product, through="OrderItem", related_name="orders")
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+    # add current date auto
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
