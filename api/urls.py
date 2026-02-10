0bc21407382e5635898b07e4a32b6baea6aeed56
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-02-10 22:43:16 +0500

    feat: create route for getting single product

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('products/', views.product_list)
+    path('products/', views.product_list),
+    path('products/<int:pk>/', views.product_detail)
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,24 @@
 from django.http import JsonResponse
 from api.serializers import ProductSerializer
 from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all() # returns queryset
     serializer = ProductSerializer(products, many=True)  # As it's more than a single object
-    return JsonResponse({
-        'data': serializer.data
-    })
+    # return JsonResponse({
+    #     'data': serializer.data
+    # })
+    # DRF Browsable API
+    # http://localhost:8000/api/v1/products/?format=json
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    # it returns product model
+    serializer = ProductSerializer(product) 
+    return Response(serializer.data)
