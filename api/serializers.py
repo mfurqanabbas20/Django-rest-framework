4e9ff41de714d715c9e75599f93341363d9eabc6
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-02-14 22:13:22 +0500

    feat: add product info view and serializer

--- api/serializers.py
+++ api/serializers.py
@@ -46,3 +46,10 @@ class OrderSerializer(serializers.ModelSerializer):
             'items',
             'total_price',
         )
+
+class ProductInfoSerializer(serializers.Serializer):
+    # get all products, count of products, max price
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
+    
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Max
 from django.http import JsonResponse
-from api.serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+from api.serializers import ProductSerializer, OrderSerializer, OrderItemSerializer, ProductInfoSerializer
 from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -27,4 +28,16 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True) 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        # It should be name represeting aggration
+        'max_price': products.aggregate(max_price=Max('price'))['max_price']
+    })
+    
     return Response(serializer.data)
